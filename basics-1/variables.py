ebe45c7ff2c317442bd739ea8e16c66991d5aac3
A-tech45 <kattelakash52@gmail.com>
2026-09-21 16:50:33 +0530

    op

---
+++ basics-1/variables.py
@@ -0,0 +1,21 @@
+a = 10 ;
+print(type(a));
+
+a = str(9)
+print(type(a))
+
+a,b,c = 10 ,20 , 30
+print(a)
+print(b)
+print(c)
+
+
+a=b=c=7
+print(a)
+print(b)
+print(c)
+
+_name = "Akash"  #can start with a underscore
+print(_name)
+
+# 1name = "akash"   will give an error as variables name dont start with number
